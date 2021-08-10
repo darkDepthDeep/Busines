035df85fe68bde339c730d310b6449cb992b552c
Arkady <Arkadii_9292@mail.ru>
2021-08-10 16:16:37 +0300

    second

--- src/js/script.js
+++ src/js/script.js
@@ -4,4 +4,63 @@ $(document).ready(function(){
         slidesToShow: 1,
         slidesToScroll: 1
       });
+      $('.expert__wrapper').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      });
+
+      new WOW().init();
+
+      // Modal
+
+      $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+      });
+      $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks').fadeOut('slow');
+      });
+
+      // validate form
+
+      function validateForm(form) {
+        $(form).validate({
+          rules: {
+            name: "required",
+            phone: "required",
+            email: {
+              required: true,
+              email: true
+            }
+          },
+          messages: {
+            name: "Пожалуйста, введите свое имя",
+            phone: "Пожалуйста, введите свой номер телефона",
+            email: {
+              required: "Пожалуйста, введите свой email",
+              email: "Неправильно введен адрес email"
+            }
+          }
+        });
+      }
+      
+      validateForm('#consultation form');
+
+      $("#phone").mask("+7 (999) 999-99-99");
+
+      $('.feed-form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+          type: "POST",
+          url: "../mailer/smart.php",
+          data: $(this).serialize()
+        }).done(function() {
+          $(this).find("input").val("");
+          $('#consultation').fadeOut();
+          $('.overlay, #thanks').fadeIn('slow');
+
+          $('.feed-form').trigger('reset');
+        });
+        return false;
+      });
   });
